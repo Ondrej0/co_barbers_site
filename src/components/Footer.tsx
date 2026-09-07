238e9b17518ae359be0099dfddc3c67591d80cb0
Ondrej0 <ondrejmuzik27@gmail.com>
2026-09-07 15:39:49 +0100

    Set up navbar and footer, add all to page layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer>
+            <p>This is a footer</p>
+            <p>email@email.com</p>
+            <p>08298 98329</p>
+        </footer>
+    )
+}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,15 @@
-
+import Link from "next/link";
 
 export function Navbar() {
     return (
-        <>
-
-        </>
+        <nav>
+            <Link href="/">Home</Link>
+            <Link href="/services">Services</Link>
+            <Link href="/barbers">Barbers</Link>
+            <Link href="/gallery">Gallery</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+            <Link href="/book">Book</Link>
+        </nav>
     )
 }
